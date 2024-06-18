a4008a4e7c271bbd58165dbb346a09842e86ddc0
XiaoQuark <kris.oldrini@gmail.com>
2024-06-18 10:36:57 +0100

    display list of all articles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Link, Route, Routes } from "react-router-dom";
+import axios from "axios";
+import { Header } from "./Header";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [articleList, setArticleList] = useState([]);
+    const [article, setArticle] = useState({});
+    const [topicList, setTopicList] = useState([]);
+    const [topic, setTopic] = useState({});
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <Header
+                setArticleList={setArticleList}
+                setTopic={setTopic}
+                topic={topic}
+            />
+            <section>
+                <Routes>
+                    <Route
+                        path='/'
+                        element={
+                            <Articles
+                                articleList={articleList}
+                                setArticleList={setArticleList}
+                                topic={topic}
+                            />
+                        }
+                    />
+                    {/* <Route path='/article_id' element='' />
+                  <Route path='/user' element='' />
+                  <Route path='/post-article' element='' /> */}
+                </Routes>
+            </section>
+            {/* <User />
+            <Footer setTopic={setTopic} topic={topic} /> */}
+        </>
+    );
 }
 
-export default App
+export default App;
+
+function Articles({ topic, articleList, setArticleList }) {
+    useEffect(() => {
+        axios
+            .get("https://xqnews.onrender.com/api/articles", {
+                params: { topic: topic },
+            })
+            .then((response) => {
+                console.log(response.data.articles[0]);
+                setArticleList(response.data.articles);
+            });
+    }, [topic]);
+    return (
+        <>
+            <div className='title-container'>
+                <h2>Feed</h2>
+            </div>
+            <ul>
+                {articleList.map((article) => {
+                    return (
+                        <ArticleCard
+                            key={article.article_id}
+                            article={article}
+                        />
+                    );
+                })}
+            </ul>
+        </>
+    );
+}
+
+function ArticleCard({ article }) {
+    return (
+        <Link
+            to={`/articles/${article.article_id}`}
+            aria-label={`navigate to ${article.title}`}
+        >
+            <li className='articles-card'>
+                <h3 className='article-card-title'>{article.title}</h3>
+                <p className='article-card-date'>{article.created_at}</p>
+                <img
+                    className='article-card-img'
+                    src={article.article_img_url}
+                    alt=''
+                />
+                <p className='article-card-username'>{article.author}</p>
+                <p className='article-card-topic'>{article.topic}</p>
+                <p className='article-card-comment-count'>
+                    {article.comment_count}
+                </p>
+                <p className='article-card-votes'>{article.votes}</p>
+            </li>
+        </Link>
+    );
+}
